19939d8028bab57262fe2f361b7d767d2c30454b
B <bsinha@ep.com>
2018-04-27 13:26:15 -0700

    fixed dict printer
    fixed dict printer

--- data/serverdict_printer.py
+++ data/serverdict_printer.py
@@ -3,12 +3,20 @@ import pickle
 import discord
 import tempfile
 import json
+import datetime
 
 server_dict_new = {}
+
+def datetime_handler(x):
+    if isinstance(x, datetime.datetime):
+        return x.isoformat()
+    raise TypeError("Unknown type")
+
+
 
 with open('../data/guilddict', "rb") as fd:
 	server_dict_old = pickle.load(fd)
 
 
 
-print(json.dumps(server_dict_old, indent=4))
+print(json.dumps(server_dict_old, default=datetime_handler, indent=4))
