90d4ebb75d7a08884da7c1d5dfce1fe7bea36a7d
TrainingB <32426796+TrainingB@users.noreply.github.com>
2018-08-21 00:13:44 -0700

    added as a ext comand

--- clembot/exts/configmanager.py
+++ clembot/exts/configmanager.py
@@ -20,6 +20,19 @@ class ConfigManager:
         self.utilities = Utilities()
 
 
+
+    @commands.command(pass_context=True, hidden=True, aliases=["list-servers"])
+    async def _list_servers(self, ctx):
+        recipient = {}
+        recipient_text = ""
+
+        for guild in ctx.bot.guilds:
+            recipient[guild.name] = guild.owner.mention
+            recipient_text += f"\n**{guild.name} [{len(guild.members)}]** - {guild.owner.name} {guild.owner.mention}"
+
+        await self.utilities._send_message(ctx.channel, recipient_text)
+
+
     @commands.group(pass_context=True, hidden=True, aliases=["setx"])
     async def _setx(self, ctx):
         if ctx.invoked_subcommand is None:
