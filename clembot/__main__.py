b7979a67ce64c7205993a7620a0bdc46559030a6
bs092016 <bsinha@ep.com>
2018-10-29 09:15:28 -0700

    risky check-in, allows bot messages to be read

--- clembot/__main__.py
+++ clembot/__main__.py
@@ -94,7 +94,7 @@ custom_error_handling(Clembot, logger)
 try:
     with open(os.path.join('data', 'guilddict'), "rb") as fd:
         Clembot.guild_dict = pickle.load(fd)
-    # print(json.dumps(Clembot.guild_dict, indent=2))
+    #print(json.dumps(Clembot.guild_dict, indent=2))
     logger.info("Serverdict Loaded Successfully")
 except OSError:
     logger.info("Serverdict Not Found - Looking for Backup")
@@ -126,12 +126,43 @@ GOOGLE_API_KEY = ""
 GOOGLE_MAPS_URL = "https://maps.googleapis.com/maps/api/staticmap?center={latlong}&markers=color:red%7C{latlong}&maptype=roadmap&size=250x125&zoom=15&key=" + GOOGLE_API_KEY
 INVITE_CODE = "AUzEXRU"
 SQLITE_DB = ""
-CACHE_VERSION = 8
+CACHE_VERSION = 9
 
 # Append path of this script to the path of
 # config files which we're loading.
 # Assumes that config files will always live in the same directory.
 script_path = os.path.dirname(os.path.realpath(__file__))
+
+
+async def process_commands(self, message):
+    """|coro|
+
+    This function processes the commands that have been registered
+    to the bot and other groups. Without this coroutine, none of the
+    commands will be triggered.
+
+    By default, this coroutine is called inside the :func:`.on_message`
+    event. If you choose to override the :func:`.on_message` event, then
+    you should invoke this coroutine as well.
+
+    This is built using other low level tools, and is equivalent to a
+    call to :meth:`~.Bot.get_context` followed by a call to :meth:`~.Bot.invoke`.
+
+    This also checks if the message's author is a bot and doesn't
+    call :meth:`~.Bot.get_context` or :meth:`~.Bot.invoke` if so.
+
+    Parameters
+    -----------
+    message: :class:`discord.Message`
+        The message to process commands for.
+    """
+    # only to keep the line bot working.
+
+    # if message.author.bot:
+    #     return
+
+    ctx = await self.get_context(message)
+    await self.invoke(ctx)
 
 
 def load_config():
