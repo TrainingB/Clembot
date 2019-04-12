4bff7ad14d0c71b16905162a7fccac78db4b12d7
TrainingB <directtob@gmail.com>
2019-04-12 13:40:03 -0700

    Update bot.py

--- clembot/bot.py
+++ clembot/bot.py
@@ -1,9 +1,35 @@
 from context import Context
 from discord.ext import commands
+from cogs.dbi import DatabaseInterface
+import logging
+
+class Bot(commands.AutoShardedBot):
+    """Custom Discord Bot class for Clembot"""
+
+    def __init__(self, **kwargs):
+        print("init()")
+        super().__init__(**kwargs)
+        # self.dbi = DatabaseInterface(self.config_details)
+        # self.loop.run_until_complete(self._db_connect())
+        self.logger = logging.getLogger('clembot.Bot')
+
+    # async def _db_connect(self):
+    #     try:
+    #         await self.dbi.start(loop=self.loop)
+    #     except asyncpg.InvalidPasswordError:
+    #         print(
+    #             'The database login is incorrect. '
+    #             'Please fix the config file and try again.')
+    #         sys.exit(0)
+    #     except asyncpg.InvalidCatalogNameError:
+    #         db_name = self.config.db_details.get('database', 'meowth')
+    #         print(
+    #             f"The database '{db_name}' was not found. "
+    #             "Please fix the config file and try again.")
+    #         sys.exit(0)
 
 
-class ClembotDiscordBot(commands.AutoShardedBot):
-    """Custom Discord Bot class for Meowth"""
+
 
     async def process_commands(self, message):
         """Processes commands that are registed with the bot and it's groups.
