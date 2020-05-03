f469966bdfb9cf4da6e16bf8c292e28f6f1870e2
TrainingB <directtob@gmail.com>
2020-05-02 23:17:43 -0700

    attempting to fix logging

--- clembot/core/logs.py
+++ clembot/core/logs.py
@@ -7,31 +7,45 @@ global_logger = None
 
 def init_loggers():
 
-    # d.py stuff
+
     global global_logger
 
     if global_logger:
         return global_logger
 
     print("init_loggers()")
-    dpy_logger = logging.getLogger("discord")
+
+
+    dpy_logfile_path = 'logs/discord.log'
+    dpy_fhandler = logging.handlers.RotatingFileHandler(
+        filename=str(dpy_logfile_path), encoding='utf-8', mode='a',
+        maxBytes=400000, backupCount=5)
+
+    dpy_logger = logging.getLogger('discord')
     dpy_logger.setLevel(logging.INFO)
+    dpy_logger.addHandler(dpy_fhandler)
+
+
+    global_logger = dpy_logger
+
+
+
     console = logging.StreamHandler()
     console.setLevel(logging.INFO)
-    dpy_logger.addHandler(console)
-    global_logger = dpy_logger
-    # Meowth
+
 
     logger = logging.getLogger("clembot")
+    logger.setLevel(logging.INFO)
 
     clembot_format = logging.Formatter(
         '%(asctime)s %(levelname)s %(module)s %(funcName)s() %(lineno)d: '
         '%(message)s',
         datefmt="[%m/%d/%Y %H:%M]")
 
+
     stdout_handler = logging.StreamHandler(sys.stdout)
     stdout_handler.setFormatter(clembot_format)
-    logger.setLevel(logging.INFO)
+
 
     logfile_path = 'logs/clembot.log'
     fhandler = logging.handlers.RotatingFileHandler(
